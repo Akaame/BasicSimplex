2ae235f6924337cb3c41ac58e46743758df86307
Akaame <Akaame@github.com>
2018-01-23 23:21:27 +0300

    Bugfix at Set Cover algorithm. Clean up repo.

--- SetCoverProblem.py
+++ SetCoverProblem.py
@@ -7,24 +7,27 @@ class SetCover:
         I = set() # satir indekslerini tutacak
         # Eklenebilecek eleman sayisi = n, o sutun icin cost = c
         # en buyuk n/c'yi bul
-        i = 0
-        while i<3:
-            i+=1
+        col_list = []
+        while True:
             I_mat = np.array(list(I), dtype=np.int)
             print "Index matrisi, ", I_mat
             mask = np.ones([rel_matrix.shape[0]], dtype=np.bool)
             mask[I_mat] = False
             print "Maskelenmis satirlar, ", rel_matrix[mask]
             cost_per_piece = np.sum(rel_matrix[mask], axis=0, dtype=np.float) / costs
-            print cost_per_piece
+            print "Parca basi masraf: ", cost_per_piece
             greatest_index = np.argmax(cost_per_piece)
             col_greatest = np.where(rel_matrix[:,greatest_index]==True)
-            print col_greatest
-            for index in col_greatest:
-                I.add(index[0])
+            col_list.append(greatest_index)
+            print "En verimli sutunun elemanlari: ", col_greatest
+            for index in col_greatest[0]:
+                I.add(index)
             if len(I) == rel_matrix.shape[0]:
                 break
-        return len(I),np.array(list(I), dtype=np.bool)
+        ret = np.zeros([rel_matrix.shape[1]], dtype=np.bool)
+        ret[col_list] = True
+        print ret
+        return len(col_list), ret
 
 class Utils:
     @staticmethod
@@ -38,11 +41,10 @@ class Utils:
             col_index = index[1]
             rel_matrix[idx][row_index] = True
             rel_matrix[idx][col_index+mat_len] = True
-        print 1./np.sum(rel_matrix, axis=0 )**2
         prob = SetCover()
         sol, sol_matrix = prob.solve(rel_matrix, np.ones(rel_matrix.shape[1]))
         
         return sol, sol_matrix[:mat_len], sol_matrix[mat_len:]
 
-mat = np.array([0,1,1,1,0,0,0,1,1]).reshape([3,3])
-print Utils.getMinimumSetCover(mat)
+# mat = np.array([0,1,1,1,0,0,0,1,1]).reshape([3,3])
+# print Utils.getMinimumSetCover(mat)
